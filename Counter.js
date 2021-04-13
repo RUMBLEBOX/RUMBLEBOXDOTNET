27319d9d5681bd532be7b02a33d03c7b3b35a02f
RUMBLEBOX_ <75748550+RUMBLEBOX@users.noreply.github.com>
2021-04-13 15:51:41 -0400

    Update Counter.js

--- Counter.js
+++ Counter.js
@@ -1,6 +1,11 @@
-const countEl = document.getElementById('counter');
+const countEl = document.getElementById('count');
+
 updateVisitCount();
 
 function updateVisitCount() {
-	fetch('https://api.countapi.xyz/update/RUMBLEBOXCOUNTER/e3d532b9-5918-4bf1-bd2b-24ca4d17d87b/?amount=1').then(res => res.json()).then(res => {countEl.innerHTML = res.value;}):
+	fetch('https://api.countapi.xyz/update/RUMBLEBOXCOUNTER/e3d532b9-5918-4bf1-bd2b-24ca4d17d87b/?amount=1')
+	.then(res => res.json())
+	.then(res => {
+		countEl.innerHTML = res.value;
+	})
 }
